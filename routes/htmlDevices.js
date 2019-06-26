d985ded9c58bf08a31e26afb82b4c1e333abbe8b
Cole Johnson <coleharrisjohnson@gmail.com>
2019-06-26 14:39:35 -0700

    added /devices

--- routes/htmlDevices.js
+++ routes/htmlDevices.js
@@ -20,30 +20,8 @@ const express = require("express");
 const router = express.Router();
 
 
-router.get("/", (req, res) => {
-    res.render("home");
-});
-
-router.get("/about", (req, res) => {
-    res.render("about");
-});
-
-// /devices requests served by htmlDevices.js
-
-//router.get("/baseband", (req, res) => {
-//    res.render("baseband");
-//});
-
-// /processor requests served by htmlProcessors.js
-
-// /fw requests served by htmlFw.js
-
-//router.get("/ota", (req, res) => {
-//    res.render("ota");
-//});
-
-router.get("/contact", (req, res) => {
-    res.render("contact");
+router.get("/devices", (req, res) => {
+    res.render("devices", require("./devices.json"));
 });
 
 
